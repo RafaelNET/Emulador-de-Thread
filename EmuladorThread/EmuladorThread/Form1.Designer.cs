039f5fc9fa5a6a0f673e7cae67cf9095eba1e9fa
Rafael de Araújo Andrade <rafael.araujo.andrade@gmail.com>
2023-02-18 02:20:15 -0300

    Criação e finalização deste projeto conceitual

---
+++ EmuladorThread/EmuladorThread/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace EmuladorThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txtNrThreadsFinalizadas = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtNrThreads = new System.Windows.Forms.TextBox();
+            this.btnNovaThred = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(163, 91);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(140, 17);
+            this.Label2.TabIndex = 9;
+            this.Label2.Text = "Threads Finalizadas:";
+            // 
+            // txtNrThreadsFinalizadas
+            // 
+            this.txtNrThreadsFinalizadas.Location = new System.Drawing.Point(166, 114);
+            this.txtNrThreadsFinalizadas.Name = "txtNrThreadsFinalizadas";
+            this.txtNrThreadsFinalizadas.ReadOnly = true;
+            this.txtNrThreadsFinalizadas.Size = new System.Drawing.Size(137, 22);
+            this.txtNrThreadsFinalizadas.TabIndex = 8;
+            this.txtNrThreadsFinalizadas.Text = "0";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(24, 91);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(124, 17);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Threads Iniciadas:";
+            // 
+            // txtNrThreads
+            // 
+            this.txtNrThreads.Location = new System.Drawing.Point(24, 114);
+            this.txtNrThreads.Name = "txtNrThreads";
+            this.txtNrThreads.ReadOnly = true;
+            this.txtNrThreads.Size = new System.Drawing.Size(124, 22);
+            this.txtNrThreads.TabIndex = 6;
+            this.txtNrThreads.Text = "0";
+            // 
+            // btnNovaThred
+            // 
+            this.btnNovaThred.Location = new System.Drawing.Point(96, 24);
+            this.btnNovaThred.Name = "btnNovaThred";
+            this.btnNovaThred.Size = new System.Drawing.Size(135, 49);
+            this.btnNovaThred.TabIndex = 5;
+            this.btnNovaThred.Text = "Novas Threads";
+            this.btnNovaThred.UseVisualStyleBackColor = true;
+            this.btnNovaThred.Click += new System.EventHandler(this.btnNovaThred_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 162);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.txtNrThreadsFinalizadas);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtNrThreads);
+            this.Controls.Add(this.btnNovaThred);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox txtNrThreadsFinalizadas;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtNrThreads;
+        internal System.Windows.Forms.Button btnNovaThred;
+    }
+}
+
---
+++ EmuladorThread/EmuladorThread/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmuladorThread
+{
+    public partial class Form1 : Form
+    {
+        //List<Thread> listaThreads = new List<Thread>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNovaThred_Click(object sender, EventArgs e)
+        {
+
+            //listaThreads.Clear();
+
+            this.Enabled = false;
+            for (var index = 1; index <= 200; index++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(minhaFuncao);
+                var novaThread = new Thread(start);
+                //listaThreads.Add(novaThread);
+                // novaThread.IsBackground = True
+                novaThread.Priority = ThreadPriority.Highest;
+                novaThread.Start(index);
+                txtNrThreads.Text = (System.Convert.ToInt32(txtNrThreads.Text) + 1).ToString();
+                txtNrThreads.Refresh();
+            }
+            this.Enabled = true;
+        }
+
+        private delegate void minhaFuncaoDelegate(object obj);
+
+        private void minhaFuncao(object obj)
+        {
+            Thread.Sleep(new Random().Next(2000, 10000));
+            ThreadFinalizada(obj);
+        }
+
+        private delegate void ThreadFinalizadaDelegate(object obj);
+        private void ThreadFinalizada(object obj)
+        {
+            if (this.txtNrThreadsFinalizadas.InvokeRequired)
+            {
+                this.Invoke(new ThreadFinalizadaDelegate(this.ThreadFinalizada), new object[] { obj });
+                return;
+            }
+
+            txtNrThreadsFinalizadas.Text = (System.Convert.ToInt32(txtNrThreadsFinalizadas.Text) + 1).ToString() ;
+            txtNrThreadsFinalizadas.Refresh();
+        }
+    }
+}
